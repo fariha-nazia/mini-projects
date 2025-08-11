5d36f4d39c13af385176fd6d1495bfa54b76a14f
Fariha Nazia <farihanazia@Mac.fritz.box>
2025-08-11 14:47:28 +0200

    the UI is ready

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -2,3 +2,11 @@ console.log('the js file added');
 
 const count = document.getElementById("count-number");
 console.log(count.innerText);
+
+function increaseCount(){
+    console.log('increase clicked');
+};
+
+function decreaseCount(){
+    console.log('decrease clicked');
+};
