d565800c59a6910c98d5bac341ce68588ee3befe
Fariha Nazia <farihanazia@Mac.fritz.box>
2025-08-20 10:38:17 +0200

    to make sure all update are saved

--- js/simple-counter.js
+++ js/simple-counter.js
@@ -3,10 +3,56 @@ console.log('the js file added');
 const count = document.getElementById("count-number");
 console.log(count.innerText);
 
-function increaseCount(){
-    console.log('increase clicked');
-};
+const counter = document.getElementById('counter');
+counter.addEventListener('click', function(){
+    console.log('clicking the whole box');
 
-function decreaseCount(){
-    console.log('decrease clicked');
-};
+    if(increaseBtn.getAttribute("disabled") == null){
+
+            // Pause the increment and decrement
+            increaseBtn.setAttribute("disabled", true);
+            decreaseBtn.setAttribute("disabled", true);
+            counter.classList.add("bg-red-500");
+    }
+    else{
+
+            increaseBtn.removeAttribute("disabled", true);
+            decreaseBtn.removeAttribute("disabled", true);
+            counter.classList.remove("bg-red-500");
+            count.innerText = 0;
+
+    }
+
+});
+
+
+// Declaring the constant
+const increaseBtn = document.getElementsByClassName('increase-btn')[0];
+const decreaseBtn = document.getElementsByClassName('decrease-btn')[0];
+
+increaseBtn.addEventListener('click', function(event){
+    // console.log('increase clicked');
+
+    // 1st: converting the string to a number
+    let currentCount = Number(count.innerText);
+    // 2nd: Doing the increment
+    currentCount++;
+    // 3rd: Setting the new value in the UI
+    count.innerText = currentCount;
+    event.stopPropagation();
+    console.log(currentCount);
+});
+
+decreaseBtn.addEventListener('click', function(event){
+     // console.log('decrease clicked');
+
+    // 1st: converting the string to a number
+    let currentCount = Number(count.innerText);
+    // Doing the decrement 
+    currentCount--;
+    // 3rd: Stetting the new value in the UI
+    count.innerText = currentCount;
+    event.stopPropagation();
+    console.log(currentCount);
+});
+
